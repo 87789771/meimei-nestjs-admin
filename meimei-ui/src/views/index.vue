dd09b8b009ca3aadaa9cfb4798fd9a4ddf3749fa
蒋胜 <87789771@qq.com>
2022-01-15 17:28:38 +0800

    提交

--- meimei-ui/src/views/index.vue
+++ meimei-ui/src/views/index.vue
@@ -1,7 +1,17 @@
 <template>
   <div class="app-container home">
-    <el-link type="primary" href="https://87789771.github.io/#/">查看开发文档请点击</el-link>
-    <el-link type="primary" href="https://github.com/87789771/meimei-nestjs-admin">查看码源请点击</el-link>
+    <h3>
+      <el-link type="primary" href="https://87789771.github.io/#/"
+        >查看开发文档请点击</el-link
+      >
+    </h3>
+    <h3>
+      <el-link
+        type="primary"
+        href="https://github.com/87789771/meimei-nestjs-admin"
+        >查看码源请点击</el-link
+      >
+    </h3>
   </div>
 </template>
 
