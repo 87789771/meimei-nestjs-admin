adededb4dd9c06fb739eec31628edf1b27155c45
蒋胜 <87789771@qq.com>
2022-01-18 10:57:35 +0800

    提交

--- meimei-admin/src/config/config.development.ts
+++ meimei-admin/src/config/config.development.ts
@@ -1,7 +1,7 @@
 /*
  * @Author: Sheng.Jiang
  * @Date: 2021-09-03 11:32:52
- * @LastEditTime: 2022-01-16 16:44:32
+ * @LastEditTime: 2022-01-18 09:40:41
  * @LastEditors: Sheng.Jiang
  * @Description: 测试环境配置文件
  * @FilePath: \meimei-admin\src\config\config.development.ts
@@ -19,7 +19,7 @@ export default defineConfig({
     host: process.env.MYSQL_HOST || 'localhost',
     port: process.env.MYSQL_PORT || 3306,
     username: process.env.MYSQL_USERNAME || 'root',
-    password: process.env.MYSQL_PASSWORD || 'admin',
+    password: process.env.MYSQL_PASSWORD || 'jiangsheng1314',
     database: process.env.MYSQL_DATABASE || 'mei-mei',
     autoLoadModels: true,
     synchronize: true,
@@ -37,5 +37,5 @@ export default defineConfig({
     port: '6379'
   },
 
-  isDemoEnvironment: false,
+  isDemoEnvironment: true,
 });
