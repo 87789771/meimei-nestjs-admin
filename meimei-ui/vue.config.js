2a962c9c202fcceddcb7f10d9ba5cecf96dbb1bc
sheng.jiang <87789771@qq.com>
2022-05-06 17:53:32 +0800

    修复路径

--- meimei-ui/vue.config.js
+++ meimei-ui/vue.config.js
@@ -15,7 +15,7 @@ const port = process.env.port || process.env.npm_config_port || 80 // 端口
 //官方vue.config.js 参考文档 https://cli.vuejs.org/zh/config/#css-loaderoptions
 // 这里只列一部分，具体配置参考文档
 module.exports = {
-  publicPath: process.env.NODE_ENV === "production" ? "./" : "./",
+  publicPath: process.env.NODE_ENV === "production" ? "/" : "./",
   // 在npm run build 或 yarn build 时 ，生成文件的目录名称（要和baseUrl的生产环境路径一致）（默认dist）
   outputDir: 'dist',
   // 用于放置生成的静态资源 (js、css、img、fonts) 的；（项目打包之后，静态资源会放在这个文件夹下）
