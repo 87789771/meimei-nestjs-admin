0c6ea31c11a71976c0617ca183c7c13355581fbe
sheng.jiang <87789771@qq.com>
2022-05-11 09:30:46 +0800

    修复 history模式问题

--- meimei-ui/src/views/monitor/job/index.vue
+++ meimei-ui/src/views/monitor/job/index.vue
@@ -180,9 +180,8 @@
                 调用方法
                 <el-tooltip placement="top">
                   <div slot="content">
-                    Bean调用示例：ryTask.ryParams('ry')
-                    <br />Class类调用示例：com.ruoyi.quartz.task.RyTask.ryParams('ry')
-                    <br />参数说明：支持字符串，布尔类型，长整型，浮点型，整型
+                    Bean调用示例：JobService.ceshi(1,2,3,true)
+                    <br />参数说明：支持字符串，布尔类型，数值
                   </div>
                   <i class="el-icon-question"></i>
                 </el-tooltip>
