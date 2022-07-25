b67e26ac6d29d13d0a3a30a950b9a447d607d9a4
sheng.jiang <87789771@qq.com>
2022-07-25 13:37:33 +0800

    blob文件上传解析文件后缀

--- meimei-admin/src/modules/common/upload/upload.module.ts
+++ meimei-admin/src/modules/common/upload/upload.module.ts
@@ -1,14 +1,11 @@
 import { UploadController } from './upload.controller';
-/*
-https://docs.nestjs.com/modules
-*/
-
 import { Module } from '@nestjs/common';
 import { MulterModule } from '@nestjs/platform-express';
 import { join } from 'path';
 import * as fs from 'fs';
 import * as moment from 'moment'
 import * as multer from 'multer';
+import * as MIMEType from 'whatwg-mimetype'
 
 
 export let storage = multer.diskStorage({
@@ -25,14 +22,26 @@ export let storage = multer.diskStorage({
         }
         // 挂载文件存储的路径
         req.query.fileName = '/upload/' + currentDate
+
         cd(null, path)
     },
     // 配置上传文件名
     filename: (req, file, cd) => {
         const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
+        const mimeType = new MIMEType(file.mimetype);
+        //获取文件后缀
+        const subtype = mimeType.subtype
+        // 拼接完整文件名
+        let originalname = ''
+        const lastIndex = file.originalname.lastIndexOf('.')
+        if (lastIndex >= 0) {
+            originalname = file.originalname.substring(0, lastIndex) + '.' + subtype
+        } else {
+            originalname = file.originalname + '.' + subtype
+        }
         // 挂载文件存储的路径
-        req.query.fileName = req.query.fileName + '/' + uniqueSuffix + '-' + file.originalname
-        cd(null, uniqueSuffix + '-' + file.originalname)
+        req.query.fileName = req.query.fileName + '/' + uniqueSuffix + '-' + originalname
+        cd(null, uniqueSuffix + '-' + originalname)
     }
 })
 
