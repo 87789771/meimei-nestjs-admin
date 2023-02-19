78d47bf6f5f40400508be13727e27a7cf59bd648
jiang.sheng <87789771@qq.com>
2023-02-19 21:51:52 +0800

    修改重定向

--- meimei-ui/src/layout/components/Navbar.vue
+++ meimei-ui/src/layout/components/Navbar.vue
@@ -109,7 +109,7 @@ export default {
       })
         .then(() => {
           this.$store.dispatch("LogOut").then(() => {
-            location.href = "/";
+            location.reload();
           });
         })
         .catch(() => {});
