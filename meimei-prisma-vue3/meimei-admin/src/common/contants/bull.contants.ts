bd614cc00e0a0247bff6cf49b1d852754c4d02fd
jiang.sheng <87789771@qq.com>
2024-11-11 21:12:06 +0800

    修改队列名

--- meimei-prisma-vue3/meimei-admin/src/common/contants/bull.contants.ts
+++ meimei-prisma-vue3/meimei-admin/src/common/contants/bull.contants.ts
@@ -1,1 +1,10 @@
-export const JOB_BULL_KEY = 'admin:job:bull';
+/*
+ * @Author: jiang.sheng 87789771@qq.com
+ * @Date: 2024-04-23 18:59:25
+ * @LastEditors: jiang.sheng 87789771@qq.com
+ * @LastEditTime: 2024-11-11 21:09:56
+ * @FilePath: /meimei-admin/src/common/contants/bull.contants.ts
+ * @Description: 
+ * 
+ */
+export const JOB_BULL_KEY = 'admin_bull_job';
