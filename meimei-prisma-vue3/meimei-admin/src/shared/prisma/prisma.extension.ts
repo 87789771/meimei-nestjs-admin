a190d7cfd8f61432c2e4d09b4133a03d2e02d360
jiang.sheng <87789771@qq.com>
2025-05-04 23:49:41 +0800

    自定义方法确定返回值类型

--- meimei-prisma-vue3/meimei-admin/src/shared/prisma/prisma.extension.ts
+++ meimei-prisma-vue3/meimei-admin/src/shared/prisma/prisma.extension.ts
@@ -1,9 +1,9 @@
 /*
  * @Author: jiang.sheng 87789771@qq.com
  * @Date: 2024-04-25 19:09:21
- * @LastEditors: JiangSheng 87789771@qq.com
- * @LastEditTime: 2024-05-11 17:26:45
- * @FilePath: \meimei-new\src\shared\prisma\prisma.extension.ts
+ * @LastEditors: jiang.sheng 87789771@qq.com
+ * @LastEditTime: 2025-05-04 23:45:29
+ * @FilePath: /meimei-admin/src/shared/prisma/prisma.extension.ts
  * @Description: 自定义数据库方法
  *
  */
@@ -12,32 +12,21 @@ import { Prisma, PrismaClient } from '@prisma/client';
 export const extendedPrismaClient = new PrismaClient().$extends({
   model: {
     $allModels: {
-      // 查询并且计数
-      async findAndCount<T, A>(
+      // 查询并且计数,支持关联查询时的类型提示
+      async findAndCount<T, Args extends Prisma.Args<T, 'findMany'>>(
         this: T,
-        args: Prisma.Args<T, 'findMany'>,
+        args: Args,
       ): Promise<{
-        rows: Prisma.Result<T, Prisma.Args<T, 'findMany'>, 'findMany'>;
+        rows: Prisma.Result<T, Args, 'findMany'>;
         total: number;
       }> {
         const context: any = Prisma.getExtensionContext(this);
-        // 不使用事务，查询会更快
         const [rows, total] = await Promise.all([
           context.findMany(args),
           context.count({ where: args.where }),
         ]);
-        // 下面是事务查询，反应慢
-        // const [rows, total] = await extendedPrismaClient.$transaction([
-        //   context.findMany(args),
-        //   context.count({ where: args.where }),
-        // ]);
-        return {
-          rows,
-          total,
-        };
-      },
-
-      async cheshi<T, A>() {},
+        return { rows, total };
+      }
     },
   },
 });
